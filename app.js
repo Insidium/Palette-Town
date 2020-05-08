71a6ecd74615b34d04ca86cbef413e37e02ee3bc
Insidium <ryanchallen@outlook.com>
2020-05-08 11:51:27 +1000

    Added slider styling for input range, added chroma colouring for sliders on hue, satuartion and brightness

--- app.js
+++ app.js
@@ -30,6 +30,17 @@ function randomColours() {
     hexText.innerText = randomColour;
     //Check for contrast
     checkTextContrast(randomColour, hexText);
+    //Initialise colourise sliders
+    // set colour to randomColour
+    const colour = chroma(randomColour);
+    //target all inputs with class 'sliders'
+    const sliders = div.querySelectorAll(".sliders input");
+    //target each slider and assign to hue, brightness and saturation
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colouriseSliders(colour, hue, brightness, saturation);
   });
 }
 
@@ -41,6 +52,28 @@ function checkTextContrast(colour, text) {
   } else {
     text.style.color = "#fff";
   }
+}
+
+function colouriseSliders(colour, hue, brightness, saturation) {
+  //Scale Saturation
+  const noSat = colour.set("hsl.s", 0); //set no saturation (lowest on scale)
+  const fullSat = colour.set("hsl.s", 1); //set full saturation (highest on scale)
+  const scaleSat = chroma.scale([noSat, colour, fullSat]); //show scale of saturation from none to full
+
+  //Scale Brightness
+  const midBright = colour.set("hsl.l", 0.5); //set mid-brightness to half as none and full are black and white already
+  const scaleBright = chroma.scale(["black", midBright, "white"]); //show scale of brightness from none to full
+
+  //Update Input Colors
+  //display saturation and brightness background as linear gradient passing in scales above
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(
+    0
+  )},${scaleBright(0.5)} ,${scaleBright(1)})`;
+  //display standard hue spectrum - like a rainbow of colours
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
 
 randomColours();
